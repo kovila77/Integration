cd18441db01dce9366cd1552e4d3b6e42718b76a
kovila77 <354981@mail.ru>
2020-03-13 18:51:27 +0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace SYAP_prac_27._02._2020
+{
+    partial class fKnapsack
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tbMaxMass = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dgvItems = new System.Windows.Forms.DataGridView();
+            this.epMain = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epMain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbMaxMass
+            // 
+            this.tbMaxMass.Location = new System.Drawing.Point(138, 232);
+            this.tbMaxMass.Name = "tbMaxMass";
+            this.tbMaxMass.Size = new System.Drawing.Size(100, 20);
+            this.tbMaxMass.TabIndex = 1;
+            this.tbMaxMass.TextChanged += new System.EventHandler(this.tbMaxMass_TextChanged);
+            this.tbMaxMass.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.tbMaxMass_KeyPress);
+            this.tbMaxMass.Validating += new System.ComponentModel.CancelEventHandler(this.tbMaxMass_Validating);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Вместимость рюкзака";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(351, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Найти решение";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(71, 14);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(294, 21);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.Validating += new System.ComponentModel.CancelEventHandler(this.comboBox1_Validating);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Алгоритм";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(382, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "добавить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dgvItems
+            // 
+            this.dgvItems.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            this.dgvItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvItems.Location = new System.Drawing.Point(12, 64);
+            this.dgvItems.Name = "dgvItems";
+            this.dgvItems.Size = new System.Drawing.Size(445, 150);
+            this.dgvItems.TabIndex = 8;
+            this.dgvItems.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvItems_CellContentClick);
+            this.dgvItems.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvItems_CellEndEdit);
+            this.dgvItems.Validating += new System.ComponentModel.CancelEventHandler(this.dgvItems_Validating);
+            // 
+            // epMain
+            // 
+            this.epMain.ContainerControl = this;
+            // 
+            // fKnapsack
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 275);
+            this.Controls.Add(this.dgvItems);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbMaxMass);
+            this.Name = "fKnapsack";
+            this.Text = "Knapsack";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItems)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.epMain)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox tbMaxMass;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgvItems;
+        private System.Windows.Forms.ErrorProvider epMain;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,267 @@
+﻿using Knapsack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+
+namespace SYAP_prac_27._02._2020
+{
+    public partial class fKnapsack : Form
+    {
+        int takeColumnIndex;
+        int massColumnIndex;
+        int costsColumnIndex;
+        int nameColumnIndex;
+
+        public fKnapsack()
+        {
+            InitializeComponent();
+            InitializeDgvItems();
+            button1.Enabled = false;
+            dgvItems.Tag = false;
+            tbMaxMass.Tag = false;
+            comboBox1.Tag = false;
+            epMain.SetError(tbMaxMass, "Обязательное поле!");
+        }
+        private void InitializeDgvItems()
+        {
+            nameColumnIndex = dgvItems.Columns.Add("name", "Название предмета");
+            massColumnIndex = dgvItems.Columns.Add("mass", "Вес");
+            costsColumnIndex = dgvItems.Columns.Add("costs", "Стоимость");
+            takeColumnIndex = dgvItems.Columns.Add(new DataGridViewTextBoxColumn
+            {
+                Name = "take",
+                HeaderText = "Брать ли?",
+                ReadOnly = true,
+                Visible = false
+            });
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var solver = ((ListBoxItem)comboBox1.SelectedItem).solver;
+            int[] m, c;
+            string answerText = "";
+            getColumnsDGV(out m, out c);
+            bool[] answer;
+            try
+            {
+                answer = solver.Solve(Convert.ToInt32(tbMaxMass.Text), m, c);
+            }
+            catch (Exception f)
+            {
+                MessageBox.Show("Непредвиденная ошибка библиотеки: " + f.Message);
+                return;
+            }
+            for (int i = 0; i < answer.Length; i++)
+            {
+                if (answer[i])
+                {
+                    dgvItems.Rows[i].Cells[takeColumnIndex].Value = "Берём";
+                    answerText += $"{i + 1},";
+                }
+                else
+                {
+                    dgvItems.Rows[i].Cells[takeColumnIndex].Value = "";
+                }
+            }
+            dgvItems.Columns[takeColumnIndex].Visible = true;
+            if (answerText.Length > 1)
+                answerText = answerText.Substring(0, answerText.Length - 1);
+            MessageBox.Show("Нужно взять следующие предметы: " + answerText);
+        }
+
+        private void getColumnsDGV(out int[] mass, out int[] costs)
+        {
+            List<int> _mass = new List<int>();
+            List<int> _costs = new List<int>();
+            foreach (DataGridViewRow row in dgvItems.Rows)
+            {
+                if (string.IsNullOrWhiteSpace(row.Cells[massColumnIndex].Value as string)
+                       && string.IsNullOrWhiteSpace(row.Cells[costsColumnIndex].Value as string))
+                { continue; }
+                _mass.Add(Convert.ToInt32(row.Cells[massColumnIndex].Value));
+                _costs.Add(Convert.ToInt32(row.Cells[costsColumnIndex].Value));
+            }
+            mass = _mass.ToArray();
+            costs = _costs.ToArray();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() != DialogResult.OK) { return; }
+            Type[] types = null;
+            try
+            {
+                types = Assembly.LoadFrom(dlg.FileName).GetTypes();
+            }
+            catch (ReflectionTypeLoadException el)
+            {
+                string res = "Выбрана некорректная библиотека\n";
+
+                Exception[] errors = el.LoaderExceptions;
+                foreach (var er in errors)
+                {
+                    res += er.Message + '\n';
+                }
+                MessageBox.Show(res);
+                return;
+            }
+            foreach (var t in types)
+            {
+                if (t.GetInterface("ISolver") == null)
+                {
+                    continue;
+                }
+                var constr = t.GetConstructor(new Type[0]);
+                if (constr != null)
+                {
+                    var solver = constr.Invoke(new object[0]) as ISolver;
+                    if (solver != null) comboBox1.Items.Add(new ListBoxItem(solver));
+                    return;
+                }
+            }
+            MessageBox.Show("В библиотеке нед подходящего класса!");
+        }
+        private void dgvItems_Validating(object sender, CancelEventArgs e) { Checkdgv(); }
+        private void dgvItems_RowValidating(object sender, DataGridViewCellCancelEventArgs e)
+        {
+        }
+
+        private void tbMaxMass_TextChanged(object sender, EventArgs e)
+        {
+            if (!tbMaxMass.Text.All(char.IsDigit))
+            {
+                epMain.SetError(tbMaxMass, "Должен содержать только цифры!");
+                tbMaxMass.Tag = false;
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(tbMaxMass.Text.Trim()))
+                {
+                    epMain.SetError(tbMaxMass, "Обязательное поле!");
+                    tbMaxMass.Tag = false;
+                }
+                else
+                {
+                    epMain.SetError(tbMaxMass, "");
+                    tbMaxMass.Tag = true;
+                }
+            }
+            CheckButton();
+        }
+
+        private void tbMaxMass_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !(e.KeyChar == (char)Keys.Delete) && !(e.KeyChar == (char)Keys.Back)) e.Handled = true;
+        }
+
+        private void tbMaxMass_Validating(object sender, CancelEventArgs e) { }
+
+        private void CheckButton()
+        {
+            button1.Enabled = (bool)dgvItems.Tag && (bool)tbMaxMass.Tag && (bool)comboBox1.Tag;
+        }
+
+        private void Checkdgv()
+        {
+            bool res = true;
+            foreach (DataGridViewRow row in dgvItems.Rows)
+            {
+                if (string.IsNullOrWhiteSpace(row.Cells[massColumnIndex].Value as string)
+                    && string.IsNullOrWhiteSpace(row.Cells[costsColumnIndex].Value as string)
+                    && string.IsNullOrWhiteSpace(row.Cells[nameColumnIndex].Value as string))
+                { continue; }
+                if (string.IsNullOrWhiteSpace(row.Cells[massColumnIndex].Value as string))
+                {
+                    row.Cells[massColumnIndex].ErrorText = "Пустое значение!";
+                    res = false;
+                }
+                if (string.IsNullOrWhiteSpace(row.Cells[costsColumnIndex].Value as string))
+                {
+                    row.Cells[costsColumnIndex].ErrorText = "Пустое значение!";
+                    res = false;
+                }
+                if (!(row.Cells[massColumnIndex].ErrorText == "")
+                    && !(row.Cells[costsColumnIndex].ErrorText == ""))
+                {
+                    res = false;
+                }
+            }
+            if (dgvItems.Rows.Count <= 1)
+            {
+                dgvItems.Tag = false;
+            }
+            else
+            {
+                dgvItems.Tag = res;
+            }
+            dgvItems.Tag = res;
+        }
+
+        private void comboBox1_Validating(object sender, CancelEventArgs e)
+        {
+            comboBox1.Tag = !(comboBox1.SelectedItem == null);
+            CheckButton();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox1.Tag = !(comboBox1.SelectedItem == null);
+            CheckButton();
+        }
+
+        private void dgvItems_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dgvItems_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            dgvItems.Columns[takeColumnIndex].Visible = false;
+            if (!(e.ColumnIndex == massColumnIndex || e.ColumnIndex == costsColumnIndex))
+            {
+                Checkdgv(); return;
+            }
+            DataGridViewCell cell = dgvItems[e.ColumnIndex, e.RowIndex];
+            if (string.IsNullOrWhiteSpace((string)cell.Value))
+            {
+                cell.ErrorText = "Пустое значение!";
+            }
+            else
+            {
+                if ((cell.ColumnIndex == massColumnIndex || cell.ColumnIndex == costsColumnIndex) && !(((string)cell.Value).All(char.IsDigit)))
+                {
+                    cell.ErrorText = "Только числа!";
+                }
+                else
+                {
+                    cell.ErrorText = "";
+                }
+            }
+            Checkdgv();
+            CheckButton();
+        }
+    }
+
+    class ListBoxItem
+    {
+        public ISolver solver;
+
+        public ListBoxItem(ISolver solver)
+        {
+            this.solver = solver;
+        }
+
+        public override string ToString()
+        {
+            return solver.GetName();
+        }
+    }
+}
